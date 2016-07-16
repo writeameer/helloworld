993cbde98a94e0c5a14d60dccfb2e04c8ce75f88
Ameer Deen <ameer.deen@industrieit.com>
2016-07-16 21:28:34 +1000

    Trying to get OSX build working

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/bin/bash -ex
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/4.4.1/lib/pkgconfig
+export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
+export AS="as -arch i386"
+export CC="cc -arch i386 -framework CoreFoundation -lobjc -liconv"
+
 xbuild /p:Configuration=Release HelloWorld.sln
 rm -f ../HelloWorld
 mkbundle -z bin/Release/*.exe -o ../HelloWorld --static --deps 
