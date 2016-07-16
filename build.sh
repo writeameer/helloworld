c8032cff30796ee54d4aa8f129acbb95b28a885a
Ameer Deen <ameer.deen@industrieit.com>
2016-07-16 21:41:00 +1000

    Updated release folder for travis

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -ex
 
+os=`uname -s`
+release_folder="../Release"
+file_name="${release_folder}/HelloWorld_${os}"
+
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/4.4.1/lib/pkgconfig
 export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
 export AS="as -arch i386"
 export CC="cc -arch i386 -framework CoreFoundation -lobjc -liconv"
 
 xbuild /p:Configuration=Release HelloWorld.sln
-rm -f ../HelloWorld
-mkbundle -z bin/Release/*.exe -o ../HelloWorld --static --deps 
+rm -rf ${release_folder}
+mkdir -p ${release_folder}
+mkbundle -z bin/Release/*.exe -o ${file_name} --static --deps 
+
+ls -la ${file_name}
