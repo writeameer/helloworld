e90760ca595484352a8903e51e1c480d58b27e02
Ameer Deen <ameer.deen@industrieit.com>
2016-07-16 23:44:40 +1000

    Test update

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
 	{
 		public static void Main(string[] args)
 		{
+			Console.WriteLine("This is a sample .NET app cross compiled to native");
 			Enumerable.Range(1, 100).ToList().ForEach(Console.WriteLine);
 		}
 	}
