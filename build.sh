382ffc394e61f409b96032777250be7ce0eea21e
Ameer Deen <ameer.deen@industrieit.com>
2016-07-16 22:07:32 +1000

    Removing path stuff from build.sh script

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ file_name="${release_folder}/HelloWorld_${os}"
 if [ "$os" = "Darwin" ];
 then
     export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/4.4.1/lib/pkgconfig
-    export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
     export AS="as -arch i386"
     export CC="cc -arch i386 -framework CoreFoundation -lobjc -liconv"
 fi
