2de3db478e37c6d3bec0ccd68e34bf1aef2972d0
Ameer Deen <ameer.deen@industrieit.com>
2016-07-17 00:09:49 +1000

    Updated counting

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace HelloWorld
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("This is a sample .NET app cross compiled to native");
-			Enumerable.Range(1, 100).ToList().ForEach(Console.WriteLine);
+			Console.WriteLine("This is a sample .NET app cross compiled to native. Watch me count to 10! :");
+			Enumerable.Range(1, 10).ToList().ForEach(Console.WriteLine);
 		}
 	}
 }
