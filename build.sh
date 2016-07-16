d28307f64ba0bd216264a973b01b536560b4cf86
Ameer Deen <ameer.deen@industrieit.com>
2016-07-16 21:54:07 +1000

    Fixing travis.yml for OSX + Linux

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@ os=`uname -s`
 release_folder="../Release"
 file_name="${release_folder}/HelloWorld_${os}"
 
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/4.4.1/lib/pkgconfig
-export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
-export AS="as -arch i386"
-export CC="cc -arch i386 -framework CoreFoundation -lobjc -liconv"
+if [ "$os" = "Darwin" ];
+then
+    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/pkgconfig:/Library/Frameworks/Mono.framework/Versions/4.4.1/lib/pkgconfig
+    export PATH=/Library/Frameworks/Mono.framework/Commands:$PATH
+    export AS="as -arch i386"
+    export CC="cc -arch i386 -framework CoreFoundation -lobjc -liconv"
+fi
 
 xbuild /p:Configuration=Release HelloWorld.sln
 rm -rf ${release_folder}
